24a6d27648eabe7888ee96cf520029b06a305b84
cdev <cuong170603@gmail.com>
2024-10-03 15:38:52 +0700

    fix:fix sequelize issues with WebPack

--- vndailynews-be/src/utils/database.js
+++ vndailynews-be/src/utils/database.js
@@ -2,15 +2,19 @@ const Sequelize = require('sequelize')
 const dotenv = require('dotenv');
 const { database } = require('../configs');
 dotenv.config();
+const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(
     database.name,
     database.username,
-    database.password, {
-    dialect: database.dialect,
-    host: database.host,
-    port: database.port,
-    timezone: '+07:00'
-});
+    database.password,
+    {
+        dialect: database.dialect,
+        dialectModule: mysql2,
+        host: database.host,
+        port: database.port,
+        timezone: '+07:00'
+    }
+);
 
 module.exports = sequelize 
